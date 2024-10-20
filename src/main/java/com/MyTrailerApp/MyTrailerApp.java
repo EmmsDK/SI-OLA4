bdb3d368b7f45a7260004671b97940b504d2c6f6
Misfosster <cph-wn11@cphbusiness.dk>
2024-10-20 16:44:40 +0200

    Eureka-server foundation added

--- src/main/java/com/MyTrailerApp/MyTrailerApp.java
+++ src/main/java/com/MyTrailerApp/MyTrailerApp.java
@@ -2,8 +2,10 @@ package com.MyTrailerApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer  // This annotation turns this application into a Eureka Server
 public class MyTrailerApp {
     public static void main(String[] args) {
         SpringApplication.run(MyTrailerApp.class, args);
