ecea4c8117fd16b8344b7dd316c8bfdeb0bff818
Misfosster <cph-wn11@cphbusiness.dk>
2024-10-21 09:47:28 +0200

    Fixed eureka dependencies

--- src/main/java/com/MyTrailerApp/MyTrailerApp.java
+++ src/main/java/com/MyTrailerApp/MyTrailerApp.java
@@ -2,7 +2,7 @@ package com.MyTrailerApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer; 
 
 @SpringBootApplication
 @EnableEurekaServer  // This annotation turns this application into a Eureka Server
